e2c45202dd5a5d921355a0617ad78b0b14bc89ff
clm <1134538020@qq.com>
2017-05-28 13:38:40 +0800

    add a file

---
+++ keshe_Ruangong/keshe_Ruangong/WenZhang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace keshe_Ruangong
+{
+    public partial class WenZhang : Form
+    {
+        public WenZhang()
+        {
+            InitializeComponent();
+        }
+    }
+}
